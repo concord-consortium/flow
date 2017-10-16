01fce8cf6e6a75ea262af8cb1a62bd7b30bf4a5a
Jason Simpson <jsimpson@concord.org>
2017-10-16 16:28:44 +0000

    Copy config file from sample_settings dir.
    [#151489214]

--- flow.sh
+++ flow.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+#
+# Start flow.py client
+#
 export PYTHONPATH=/home/pi/rhizo
+
+CONFIG_FILE=config.hjson
+
 cd /home/pi/flow
+
+if [ ! -f $CONFIG_FILE ]; then
+    cp sample_settings/$CONFIG_FILE $CONFIG_FILE
+fi
+
 python flow.py
 read -rsp $'Press enter to continue...\n'
