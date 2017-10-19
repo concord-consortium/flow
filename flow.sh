732c9de32adb6d34b6e21a9119a0a7789c27d2df
Jason Simpson <jsimpson@concord.org>
2017-10-19 14:18:59 +0000

    Remove sample_settings/config.hjson
    [#151489214]

--- flow.sh
+++ flow.sh
@@ -3,14 +3,6 @@
 # Start flow.py client
 #
 export PYTHONPATH=/home/pi/rhizo
-
-CONFIG_FILE=config.hjson
-
 cd /home/pi/flow
-
-if [ ! -f $CONFIG_FILE ]; then
-    cp sample_settings/$CONFIG_FILE $CONFIG_FILE
-fi
-
 python flow.py
 read -rsp $'Press enter to continue...\n'
